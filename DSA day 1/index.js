20cdaa3ba12a087653c51993011b752a45dca1be
srijan <srijan.dhl21@gmail.com>
2024-02-20 21:34:09 +1100

    done day 1

--- DSA day 1/index.js
+++ DSA day 1/index.js
@@ -1,19 +1,61 @@
-let numMap = {};
+'use strict';
 
-// Iterate through the array
-for (let i = 0; i < nums.length; i++) {
-  // Calculate the complement needed to reach the target
-  let complement = target - nums[i];
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
 
-  // Check if the complement is already in the object
-  if (numMap.hasOwnProperty(complement)) {
-    // If found, return the indices of the two numbers
-    return [numMap[complement], i];
-  }
+let inputString = '';
+let currentLine = 0;
 
-  // If not found, store the current number and its index in the object
-  numMap[nums[i]] = i;
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
 }
 
-// If no solution is found, return an empty array or handle it as needed
-return [];
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function plusMinus(arr) {
+    // Write your code here
+    const nums = {
+        'positive':0,
+        'negative':0,
+        'zero':0,
+    };
+    arr.forEach(element => {
+        if(element > 0){
+            nums['positive'] += 1;
+        }
+        else if (element < 0) {
+            nums['negative'] += 1;
+        } else {
+            nums['zero'] += 1;
+        }
+    });
+
+    const keys = Object.keys(nums);
+    keys.forEach(key => {
+        const ratio = nums[key] / arr.length;
+        console.log(ratio.toFixed(6));
+    });
+
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    plusMinus(arr);
+}
--- Readme.md
+++
@@ -1,1 +0,0 @@
-This is DSA files from Day 1
